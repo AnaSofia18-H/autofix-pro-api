abf5af9a569764c362801c7b5fa7b0df7ec824ca
Julio Valencia <jhvo14@gmail.com>
2026-09-04 18:29:06 -0500

    feat(mecanicos): agregar DTOs servicio y controlador

--- src/mecanicos/mecanicos.controller.ts
+++ src/mecanicos/mecanicos.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { MecanicosService } from './mecanicos.service';
+import { CreateMecanicoDto } from './dto/create-mecanico.dto';
+import { UpdateMecanicoDto } from './dto/update-mecanico.dto';
 
 @Controller('mecanicos')
-export class MecanicosController {}
+export class MecanicosController {
+  constructor(private readonly mecanicosService: MecanicosService) {}
+
+  @Post()
+  create(@Body() dto: CreateMecanicoDto) {
+    return this.mecanicosService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.mecanicosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.mecanicosService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateMecanicoDto,
+  ) {
+    return this.mecanicosService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.mecanicosService.remove(id);
+  }
+}
