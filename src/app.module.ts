775bcb743a7b908bcfe56b42829abb95b1d7fc58
Ana Sofia Hoyos <ana.sha18@outlook.com>
2026-09-10 23:04:06 -0500

    feat: conecta todos los modulos del proyecto en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
-import { MecanicosModule } from './mecanicos/mecanicos.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MarcasModule } from './marcas/marcas.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+// Importaciones de los módulos de tus compañeros
+import { MecanicosModule } from './mecanicos/mecanicos.module';
+import { MarcasModule } from './marcas/marcas.module';
+import { ModelosModule } from './modelos/modelos.module';
+import { VehiculosModule } from './vehiculos/vehiculos.module';
+import { RepuestosModule } from './repuestos/repuestos.module';
+import { OrdenesServicioModule } from './ordenes-servicio/ordenes-servicio.module';
 
 @Module({
-  imports: [MecanicosModule,
+  imports: [
+    MecanicosModule,
+    MarcasModule,
+    ModelosModule,
+    VehiculosModule,
+    RepuestosModule,
+    OrdenesServicioModule,
     ConfigModule.forRoot({
       isGlobal: true, // Hace que las variables estén disponibles en todo el proyecto
     }),
@@ -19,7 +32,9 @@ import { AppController } from './app.controller';
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-    }), MarcasModule
+    }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
