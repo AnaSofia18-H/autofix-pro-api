436bb49469432843de724e7c0e1e5b67db30f17c
Ana Sofia Hoyos <ana.sha18@outlook.com>
2026-09-10 23:06:22 -0500

    fix: agrega TypeOrmModule.forFeature en vehiculos.module

--- src/vehiculos/vehiculos.module.ts
+++ src/vehiculos/vehiculos.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { VehiculosService } from './vehiculos.service';
 import { VehiculosController } from './vehiculos.controller';
+import { Vehiculo } from './entities/vehiculo.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Vehiculo]) // <- Esto es lo que le hacía falta
+  ],
+  controllers: [VehiculosController],
   providers: [VehiculosService],
-  controllers: [VehiculosController]
+  exports: [VehiculosService]
 })
 export class VehiculosModule {}
