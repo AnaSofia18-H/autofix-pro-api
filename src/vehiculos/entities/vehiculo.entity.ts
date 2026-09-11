0e7fd4b8de2e8dd74f2f49e894243ad52131afd3
Ana Sofia Hoyos <ana.sha18@outlook.com>
2026-09-10 21:25:15 -0500

    fix: resuelve conflicto en entidad de vehiculo

--- src/vehiculos/entities/vehiculo.entity.ts
+++ src/vehiculos/entities/vehiculo.entity.ts
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Modelo } from '../../modelos/entities/modelo.entity';
- 
-@Entity('vehiculos')
-export class Vehiculo {
-  @PrimaryGeneratedColumn()
-  id: number;
- 
-  @Column()
-  placa: string;
- 
-  @Column()
-  color: string;
- 
-  @Column()
-  anio: number;
- 
-  @ManyToOne(() => Modelo)
-  @JoinColumn({ name: 'modelo_id' })
-  modelo: Modelo;
-=======
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('vehiculos')
 export class Vehiculo {
     @PrimaryGeneratedColumn()
     id: number;
->>>>>>> main
+
+    @Column()
+    placa: string;
+
+    @Column()
+    color: string;
+
+    @Column()
+    anio: number;
+
+    @ManyToOne(() => Modelo)
+    @JoinColumn({ name: 'modelo_id' })
+    modelo: Modelo;
 }
